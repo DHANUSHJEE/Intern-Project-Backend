77caa87a0dbd54ce73cb36a0ee7985d0f5d2d3c5
Dhanush J S <dhanushjayakumar2@gmail.com>
2024-09-19 21:46:31 +0530

    updated

--- DataBase/database.js
+++ DataBase/database.js
@@ -6,11 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.CONNECTIONSTRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 30000,
-        });
+        const conn = await mongoose.connect(process.env.CONNECTIONSTRING);
         console.log(`MongoDB Connected`);
     } catch (error) {   
         console.log(error,"Error in DB connection");
