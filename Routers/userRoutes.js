ef55e2c6233eac77c13b79e5b1ad5e68a88bbcb1
Dhanush J S <dhanushjayakumar2@gmail.com>
2024-09-22 10:22:21 +0530

    updated

--- Routers/userRoutes.js
+++ Routers/userRoutes.js
@@ -2,8 +2,22 @@ import express from "express";
 import userController from "../Controllers/userControllers.js";
 import auth from "../Auth/auth.js";
 import { upload } from '../Controllers/userControllers.js'; 
+import jwt from "jsonwebtoken"
 
 const router = express.Router();
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Bearer token
+
+    if (token == null) return res.sendStatus(401); // No token, return 401
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403); // Invalid token, return 403
+        req.user = user;
+        next();
+    });
+};
 
 //Register
 router.post("/signup", userController.register )
@@ -18,7 +32,7 @@ router.post("/forgotpassword",userController.forgotPassword)
 router.post('/addEmployee',auth.verifyToken,upload, userController.addEmployee)
 
 //get employees
-router.get('/getAllEmployees',auth.verifyToken, userController.getAllEmployees)
+router.get('/getAllEmployees',authenticateToken, userController.getAllEmployees)
 
 //edit employess
 router.put('/updateEmployee/:id',auth.verifyToken, userController.updateEmployee)
