5cab9de7bdd62d1cc945c44478ccabf658bb88fc
Dhanush J S <dhanushjayakumar2@gmail.com>
2024-09-22 10:14:04 +0530

    updated

--- Schema/employeeSchema.js
+++ Schema/employeeSchema.js
@@ -1,45 +1,15 @@
 import mongoose from "mongoose";
 
 const employeeSchema = new mongoose.Schema({
-    no: {
-        type: 'number',
-        require: 'true',
-        unique:'true'
-    },
-    name:{
-        type: 'string',
-        required: true,
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    mobile: { type: String, required: true },
+    designation: { type: String, required: true },
+    gender: { type: String, required: true },
+    courses: { type: [String], required: true },
+    image: { type: String, required: false }
+});
 
-    },
-    email: {
-        type: 'string',
-        required: true,
-        unique: true,
-
-    },
-    mobile: {
-        type: 'number',
-        required: true,
-        unique: true
-    },
-    designation: {
-        type: String,
-        enum: ['HR', 'Manager', 'Sales'], // Only these values are allowed
-        required: true
-        
-    },
-    gender: {
-        type: 'string',
-        enum: ["Male", "Female"],
-        required: true
-    },
-    course: {
-        type: 'string',
-        enum: ['MCA', 'BCA', 'BSC'],
-        required: true
-    }
-
-})
 
 
 export default mongoose.model("Employee", employeeSchema)
